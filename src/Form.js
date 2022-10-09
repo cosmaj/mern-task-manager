d2549b0bdcfc47c840529f3fd499176769b54369
cosmaj <cosmaj0411@gmail.com>
2022-10-09 13:52:27 +0300

    Modified hook

--- src/Form.js
+++ src/Form.js
@@ -2,17 +2,14 @@ import React, { useState } from "react";
 
 const Form = () => {
 
-    const [inputs, setInputs] = useState({})
+    const [taskName, setTaskName] = useState('')
+    const [duration, setDuration] = useState('')
+    const [completion, setCompletion] = useState(false)
 
-    const handleChange = (e)=>{
-        const name = e.target.name
-        const value  =  e.target.value
-        setInputs(values => ({...values,  [name]: value }))
-    }
 
     const handleFormSubmit = (e)=>{
         e.preventDefault();
-        console.log(inputs)
+        console.log(taskName, duration, completion)
     }
 
     return(
@@ -22,21 +19,26 @@ const Form = () => {
                 <form className='w-100' id='taskForm' onSubmit={ handleFormSubmit }>
                     <div className='mb-3'>
                         <label htmlFor='taskName' className='form-label'>Task</label>
-                        <input type='text' className='form-control' name='userName' value={ inputs.userName || ""} onChange={ handleChange }/>
+                        <input type='text' className='form-control' name='taskName' value={ taskName } onChange={ (e)=> setTaskName(e.target.value) }/>
                     </div>
                     <div className='mb-3'>
                         <label htmlFor='duration' className='form-label'>Duration in minutes</label>
-                        <input type='number' className='form-control' name='duration' value={inputs.duration || ""} onChange={ handleChange } />
+                        <input type='number' className='form-control' name='duration' value={ duration } onChange={ (e)=> setDuration(e.target.value) } />
                     </div>
                     <div className='form-check form-switch mb-3'>
                         <label htmlFor='completion' className='form-check-label'>Task completed?</label>
-                        <input type='checkbox' className='form-check-input' name='completion' value={inputs.completion || ""} role='switch' onChange={ handleChange }/>
+                        <input type='checkbox' className='form-check-input' name='completion' value={ completion } role='switch' onChange={ (e)=> setCompletion(e.target.checked ? true: false) }/>
                     </div>
                     <div className='mb-3'>
                         <input type='reset' className='btn btn-danger m-2' value='Reset' />
-                        <button type='button' className='btn btn-primary m-2' >Save</button>
+                        <button type='submit' className='btn btn-primary m-2' >Save</button>
                         <button type='button' className='btn btn-success m-2' >Delete</button>
                     </div>
+                    <p>
+                        Task: { taskName } <br />
+                        Duration: { duration } <br />
+                        Completion: { completion }
+                    </p>
                 </form>
             </div>
         </div>
